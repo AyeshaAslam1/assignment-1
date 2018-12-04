826fa06208f62cae5541288e4aaad13176405452
AyeshaAslam1 <45604608+AyeshaAslam1@users.noreply.github.com>
2018-12-04 23:22:07 +0500

    Add files via upload

---
+++ linkedlist.h
@@ -0,0 +1,17 @@
+#ifndef linkedlist_H
+#define linkedlist_H
+#include "complex_number.h"
+class linkedlist
+{
+private:
+	complex_number* head;
+public:
+	linkedlist ();
+	~linkedlist ();
+	void insertend(complex_number num);
+	void deletepos (int x);
+	void insertafterpos (complex_number num, int x);
+	void traverse ();
+};
+
+#endif
